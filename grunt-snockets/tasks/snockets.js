21e4f54c2eaf7073a682180e25358690a6170437
Hector Zarco <hezarco@gmail.com>
2014-05-07 11:51:03 +0200

    Add Bower package

--- grunt-snockets/tasks/snockets.js
+++ grunt-snockets/tasks/snockets.js
@@ -11,8 +11,6 @@ var Snockets = require('snockets'),
  */
 
 module.exports = function(grunt) {
-  var snockets = new Snockets();
-
   // Please see the grunt documentation for more information regarding task and
   // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
 
@@ -21,31 +19,46 @@ module.exports = function(grunt) {
   // ==========================================================================
 
   grunt.registerMultiTask('snockets', 'Building js files with snockets.js.', function() {
-    // It doesn't run with empty src and dest parameters.    
-    if (typeof this.data.src === 'undefined' ||
-        typeof this.data.dest === 'undefined') {
-      grunt.log.error('Missing Options: src and dest options necessary');
-      return false;
-    }
+    this.snocketsOptions = {
+      async: (this.data.async || false),
+      bare: (this.data.bare || false)
+    };
 
-    if (fs.existsSync(path.resolve(this.data.src))) {
-      try {
-        js = snockets.getConcatenation(this.data.src, {async: false});
-
-        if (this.data.banner)
-          js = this.data.banner + '\n' + js;
-
-        fs.writeFileSync(path.resolve(this.data.dest), js);
-
-        grunt.log.write(this.data.src + ' snocket to ' + this.data.dest);
-        return true;
-      } catch (e) {
-        grunt.log.error(e);
+    var snockets = new Snockets(this.snocketsOptions);
+    var processSnocket = function(snocket_data) {
+      // It doesn't run with empty src and dest parameters.
+      if (typeof snocket_data.src === 'undefined' ||
+          typeof snocket_data.dest === 'undefined') {
+        grunt.log.error('Missing Options: src and dest options necessary');
         return false;
       }
+
+      if (fs.existsSync(path.resolve(snocket_data.src))) {
+        try {
+          js = snockets.getConcatenation(snocket_data.src, this.snocketsOptions);
+
+          if (snocket_data.banner)
+            js = snocket_data.banner + '\n' + js;
+
+          fs.writeFileSync(path.resolve(snocket_data.dest), js);
+
+          grunt.log.writeln('Compiled ' + snocket_data.src + ' to ' + snocket_data.dest);
+
+          return true;
+        } catch (e) {
+          grunt.log.error(e);
+          return false;
+        }
+      } else {
+        grunt.log.error('Missing File: ' + snocket_data.src);
+        return false;
+      }
+    };
+
+    if (Array.isArray(this.data)) {
+      this.data.forEach(processSnocket);
     } else {
-      grunt.log.error('Missing File: ' + this.data.src);
-      return false;
+      processSnocket(this.data);
     }
   });
 
